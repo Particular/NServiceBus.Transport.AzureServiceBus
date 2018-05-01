ba98345f16ae5117964cd0e579ff9e01ff1c143f
Sean Feldman <feldman.sean@gmail.com>
2018-04-30 23:11:28 -0600

    Initial solution structure

---
+++ src/AcceptanceTests/ConfigureEndpointAzureServiceBusTransport.cs
@@ -0,0 +1,18 @@
+﻿namespace NServiceBus.Transport.AzureServiceBus.AcceptanceTests
+{
+    using System.Threading.Tasks;
+    using AcceptanceTesting.Support;
+
+    public class ConfigureEndpointAzureServiceBusTransport : IConfigureEndpointTestExecution
+    {
+        public Task Configure(string endpointName, EndpointConfiguration configuration, RunSettings settings, PublisherMetadata publisherMetadata)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Cleanup()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/AcceptanceTests/TestSuiteConstraints.cs
@@ -0,0 +1,22 @@
+﻿namespace NServiceBus.AcceptanceTests
+{
+    using AcceptanceTesting.Support;
+    using Transport.AzureServiceBus.AcceptanceTests;
+
+    public partial class TestSuiteConstraints
+    {
+        public bool SupportsDtc => false;
+
+        public bool SupportsCrossQueueTransactions => false;
+
+        public bool SupportsNativePubSub => true;
+
+        public bool SupportsNativeDeferral => true;
+
+        public bool SupportsOutbox => true;
+
+        public IConfigureEndpointTestExecution CreateTransportConfiguration() => new ConfigureEndpointAzureServiceBusTransport();
+
+        public IConfigureEndpointTestExecution CreatePersistenceConfiguration() => new ConfigureEndpointInMemoryPersistence();
+    }
+}
