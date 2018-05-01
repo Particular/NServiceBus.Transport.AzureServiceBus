bc5efd7056a55bb711e36461d3fcc4e80d566c96
Sean Feldman <feldman.sean@gmail.com>
2018-05-01 16:21:54 -0600

    Add initial transport definition and infrastructure
    Co-authored-by: Brandon Ording <bording@gmail.com>
    Co-authored-by: Sean Feldman <feldman.sean@gmail.com>

---
+++ src/Transport/AzureServiceBusTransportInfrastructure.cs
@@ -0,0 +1,80 @@
+﻿namespace NServiceBus.Transport.AzureServiceBus
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading.Tasks;
+    using DelayedDelivery;
+    using Performance.TimeToBeReceived;
+    using Routing;
+    using Settings;
+    using Transport;
+
+    class AzureServiceBusTransportInfrastructure : TransportInfrastructure
+    {
+        readonly SettingsHolder settings;
+        readonly string connectionString;
+
+        public AzureServiceBusTransportInfrastructure(SettingsHolder settings, string connectionString)
+        {
+            this.settings = settings;
+            this.connectionString = connectionString;
+        }
+
+        public override TransportReceiveInfrastructure ConfigureReceiveInfrastructure()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override TransportSendInfrastructure ConfigureSendInfrastructure()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override TransportSubscriptionInfrastructure ConfigureSubscriptionInfrastructure()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override EndpointInstance BindToLocalEndpoint(EndpointInstance instance) => instance;
+
+        public override string ToTransportAddress(LogicalAddress logicalAddress)
+        {
+            var queue = new StringBuilder(logicalAddress.EndpointInstance.Endpoint);
+
+            if (logicalAddress.EndpointInstance.Discriminator != null)
+            {
+                queue.Append($"-{logicalAddress.EndpointInstance.Discriminator}");
+            }
+
+            if (logicalAddress.Qualifier != null)
+            {
+                queue.Append($".{logicalAddress.Qualifier}");
+            }
+
+            return queue.ToString();
+        }
+
+        public override Task Start()
+        {
+            return base.Start();
+        }
+
+        public override Task Stop()
+        {
+            return base.Stop();
+        }
+
+        public override IEnumerable<Type> DeliveryConstraints => new List<Type>
+        {
+            typeof(DelayDeliveryWith),
+            typeof(DoNotDeliverBefore),
+            typeof(DiscardIfNotReceivedBefore)
+        };
+
+        // TODO: Change to SendsAtomicWithReceive when https://github.com/Azure/azure-service-bus-dotnet/issues/423 is closed
+        public override TransportTransactionMode TransactionMode => TransportTransactionMode.ReceiveOnly;
+
+        public override OutboundRoutingPolicy OutboundRoutingPolicy => new OutboundRoutingPolicy(OutboundRoutingType.Unicast, OutboundRoutingType.Multicast, OutboundRoutingType.Unicast);
+    }
+}
