f649b1b5e699ad08c89bd60df1d3c950cbf44350
Sean Feldman <feldman.sean@gmail.com>
2018-06-01 16:21:40 -0600

    Test MessageSenderPool that caching works
    Co-authored-by: Brandon Ording <bording@gmail.com>
    Co-authored-by: Sean Feldman <feldman.sean@gmail.com>

---
+++ src/Tests/Sending/MessageSenderPoolTests.cs
@@ -0,0 +1,22 @@
+﻿namespace NServiceBus.Transport.AzureServiceBus.Tests.Sending
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class MessageSenderPoolTests
+    {
+        [Test]
+        public void Should_get_cached_sender()
+        {
+            var pool = new MessageSenderPool("Endpoint=sb://fake.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=fake=");
+
+            var firstMessageSender = pool.GetMessageSender("dest");
+
+            pool.ReturnMessageSender(firstMessageSender);
+
+            var secondMessageSender = pool.GetMessageSender("dest");
+
+            Assert.AreEqual(firstMessageSender, secondMessageSender);
+        }
+    }
+}
