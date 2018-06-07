b62f118a74157b8e5c5021ee1ce8ec1838e79ff3
Sean Feldman <feldman.sean@gmail.com>
2018-06-07 17:17:03 -0600

    Add Task extensions
    Co-authored-by: Brandon Ording <bording@gmail.com>
    Co-authored-by: Sean Feldman <feldman.sean@gmail.com>

---
+++ src/Transport/TaskEx.cs
@@ -0,0 +1,11 @@
+﻿namespace NServiceBus.Transport.AzureServiceBus
+{
+    using System.Threading.Tasks;
+
+    static class TaskEx
+    {
+        public static void Ignore(this Task task)
+        {
+        }
+    }
+}
