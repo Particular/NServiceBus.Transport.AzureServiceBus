6a23fa2acdd8c15fb99587a160b227271e4ab555
Sean Feldman <feldman.sean@gmail.com>
2018-06-11 16:14:22 -0600

    Add MessageReceiver extension methods
    Co-authored-by: Brandon Ording <bording@gmail.com>
    Co-authored-by: Sean Feldman <feldman.sean@gmail.com>

---
+++ src/Transport/Receiving/MessageReceiverExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace NServiceBus.Transport.AzureServiceBus
+{
+    using System.Threading.Tasks;
+    using Microsoft.Azure.ServiceBus.Core;
+
+    static class MessageReceiverExtensions
+    {
+        public static Task SafeCompleteAsync(this MessageReceiver messageReceiver, TransportTransactionMode transportTransactionMode, string lockToken)
+        {
+            if (transportTransactionMode != TransportTransactionMode.None)
+            {
+                return messageReceiver.CompleteAsync(lockToken);
+            }
+
+            return Task.CompletedTask;
+        }
+        public static Task SafeAbandonAsync(this MessageReceiver messageReceiver, TransportTransactionMode transportTransactionMode, string lockToken)
+        {
+            if (transportTransactionMode != TransportTransactionMode.None)
+            {
+                return messageReceiver.AbandonAsync(lockToken);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
