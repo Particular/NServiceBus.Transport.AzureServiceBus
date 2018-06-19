47fe458d353b04fc4c8c953eac3408c50d0076c1
Sean Feldman <feldman.sean@gmail.com>
2018-06-19 15:28:23 -0600

    Define commands skeleton for the .NET Core global tool
    Co-authored-by: Brandon Ording <bording@gmail.com>
    Co-authored-by: Sean Feldman <feldman.sean@gmail.com>

--- src/CommandLine/Program.cs
+++ src/CommandLine/Program.cs
@@ -1,11 +1,76 @@
 ﻿namespace NServiceBus.Transport.AzureServiceBus.CommandLine
 {
     using System;
+    using McMaster.Extensions.CommandLineUtils;
 
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            var app = new CommandLineApplication
+            {
+                Name = "asb-transport"
+            };
+
+            app.HelpOption(inherited: true);
+
+            app.Command("endpoint", endpointCommand =>
+            {
+                endpointCommand.OnExecute(() =>
+                {
+                    Console.WriteLine("Specify a subcommand");
+                    endpointCommand.ShowHelp();
+                    return 1;
+                });
+
+                endpointCommand.Command("create", createCommand =>
+                {
+                    createCommand.Description = "Creates required infrastructure for an endpoint.";
+                    var name = createCommand.Argument("name", "Name of the endpoint (required)").IsRequired();
+
+                    var topicName = createCommand.Option("-t|--topic", "Topic name (defaults to 'bundle-1')", CommandOptionType.SingleValue);
+                    var subscriptionName = createCommand.Option("-s|--subscription", "Subscription name (defaults to endpoint name) ", CommandOptionType.SingleValue);
+
+                    createCommand.OnExecute(() =>
+                    {
+                        Console.WriteLine($"Endpoint name '{name.Value}', topic name '{(topicName.HasValue() ? topicName.Value() : "bundle-1")}', " +
+                                          $"subscription name '{(subscriptionName.HasValue() ? subscriptionName.Value() : name.Value)}'");
+
+                    });
+                });
+            });
+
+            app.Command("queue", queueCommand =>
+            {
+                queueCommand.OnExecute(() =>
+                {
+                    Console.WriteLine("Specify a subcommand");
+                    queueCommand.ShowHelp();
+                    return 1;
+                });
+
+                queueCommand.Command("create", createCommand =>
+                {
+                    createCommand.Description = "Creates a queue with the settings required by the transport.";
+                    var name = createCommand.Argument("name", "Name of the queue (required)").IsRequired();
+                    
+                    var size = createCommand.Option<int>("-s|--size", "Queue size in GB (defaults to 5)", CommandOptionType.SingleValue);
+
+                    createCommand.OnExecute(() =>
+                    {
+                        Console.WriteLine($"Queue name '{name.Value}', size '{(size.HasValue() ? size.ParsedValue : 5)}'");
+                    });
+                });
+            });
+
+            app.OnExecute(() =>
+            {
+                Console.WriteLine("Specify a subcommand");
+                app.ShowHelp();
+                return 1;
+            });
+
+            return app.Execute(args);
         }
     }
 }
