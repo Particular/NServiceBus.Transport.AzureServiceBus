b4ec90b75146751723ec71f28d4c6b89cffa3953
Sean Feldman <feldman.sean@gmail.com>
2018-06-19 17:35:24 -0600

    Implemented endpoint-create command
    Co-authored-by: Brandon Ording <bording@gmail.com>
    Co-authored-by: Sean Feldman <feldman.sean@gmail.com>

--- src/CommandLine/Program.cs
+++ src/CommandLine/Program.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Threading.Tasks;
     using McMaster.Extensions.CommandLineUtils;
+    using Microsoft.Azure.ServiceBus;
     using Microsoft.Azure.ServiceBus.Management;
 
     class Program
@@ -32,16 +33,81 @@
                     createCommand.Description = "Creates required infrastructure for an endpoint.";
                     var name = createCommand.Argument("name", "Name of the endpoint (required)").IsRequired();
 
+                    var size = createCommand.Option<int>("-s|--size", "Queue size in GB (defaults to 5)", CommandOptionType.SingleValue);
+                    var partitioning = createCommand.Option("-p|--partitioned", "Enable partitioning", CommandOptionType.NoValue);
                     var topicName = createCommand.Option("-t|--topic", "Topic name (defaults to 'bundle-1')", CommandOptionType.SingleValue);
-                    var subscriptionName = createCommand.Option("-s|--subscription", "Subscription name (defaults to endpoint name) ", CommandOptionType.SingleValue);
+                    var subscriptionName = createCommand.Option("-b|--subscription", "Subscription name (defaults to endpoint name) ", CommandOptionType.SingleValue);
 
-                    createCommand.OnExecute(() =>
+                    createCommand.OnExecute(async () =>
                     {
-                        Console.WriteLine($"Endpoint name '{name.Value}', topic name '{(topicName.HasValue() ? topicName.Value() : "bundle-1")}', " +
+                        var topicNameToUse = topicName.HasValue() ? topicName.Value() : "bundle-1";
+                        var subscriptionNameToUse = subscriptionName.HasValue() ? subscriptionName.Value() : name.Value;
+
+                        await Run(async client =>
+                        {
+                            try
+                            {
+                                await CreateQueue(client, name, size, partitioning);
+                            }
+                            catch (MessagingEntityAlreadyExistsException)
+                            {
+                                Console.WriteLine("Queue already exists, skipping creation");
+                            }
+
+                            var topicDescription = new TopicDescription(topicNameToUse)
+                            {
+                                EnableBatchedOperations = true,
+                                EnablePartitioning = partitioning.HasValue(),
+                                MaxSizeInMB = (size.HasValue() ? size.ParsedValue : 5) * 1024,
+                            };
+
+                            try
+                            {
+                                await client.CreateTopicAsync(topicDescription);
+                            }
+                            catch (MessagingEntityAlreadyExistsException)
+                            {
+                                Console.WriteLine("Topic already exists, skipping creation");
+                            }
+
+                            var subscriptionDescription = new SubscriptionDescription(topicNameToUse, subscriptionNameToUse)
+                            {
+                                LockDuration = TimeSpan.FromMinutes(5),
+                                ForwardTo = name.Value,
+                                EnableDeadLetteringOnFilterEvaluationExceptions = false,
+                                MaxDeliveryCount = int.MaxValue,
+                                // TODO: uncomment when https://github.com/Azure/azure-service-bus-dotnet/issues/499 is fixed
+                                //EnableBatchedOperations = true,
+                                // TODO: https://github.com/Azure/azure-service-bus-dotnet/issues/501 is fixed
+                                //UserMetadata = name.Value
+                            };
+
+                            try
+                            {
+                                await client.CreateSubscriptionAsync(subscriptionDescription);
+                            }
+                            catch (MessagingEntityAlreadyExistsException)
+                            {
+                                Console.WriteLine("Subscription already exists, skipping creation");
+                            }
+
+                            try
+                            {
+                                // TODO: remove when https://github.com/Azure/azure-service-bus-dotnet/issues/502 is implemented
+                                await client.DeleteRuleAsync(topicNameToUse, subscriptionNameToUse, RuleDescription.DefaultRuleName);
+                            }
+                            catch (MessagingEntityNotFoundException)
+                            {
+                            }
+                        });
+
+                        Console.WriteLine($"Endpoint name '{name.Value}', topic name '{topicNameToUse}', " +
                                           $"subscription name '{(subscriptionName.HasValue() ? subscriptionName.Value() : name.Value)}'");
 
                     });
                 });
+
+                // TODO: implement "endpoint show subscriptions"
             });
 
             app.Command("queue", queueCommand =>
@@ -63,19 +129,7 @@
                     
                     createCommand.OnExecute(async () =>
                     {
-                        await Run(client =>
-                        {
-                            var queueDescription = new QueueDescription(name.Value)
-                            {
-                                EnableBatchedOperations = true,
-                                LockDuration = TimeSpan.FromMinutes(5),
-                                MaxDeliveryCount = int.MaxValue,
-                                MaxSizeInMB = (size.HasValue() ? size.ParsedValue : 5) * 1024,
-                                EnablePartitioning = partitioning.HasValue()
-                            };
-
-                            return client.CreateQueueAsync(queueDescription);
-                        });
+                        await Run(client => CreateQueue(client, name, size, partitioning));
 
                         Console.WriteLine($"Queue name '{name.Value}', size '{(size.HasValue() ? size.ParsedValue : 5)}GB', partitioned '{partitioning.HasValue()}' created");
                     });
@@ -114,6 +168,20 @@
 
                 await client.CloseAsync();
             }
+        }
+
+        static Task CreateQueue(ManagementClient client, CommandArgument name, CommandOption<int> size, CommandOption partitioning)
+        {
+            var queueDescription = new QueueDescription(name.Value)
+            {
+                EnableBatchedOperations = true,
+                LockDuration = TimeSpan.FromMinutes(5),
+                MaxDeliveryCount = int.MaxValue,
+                MaxSizeInMB = (size.HasValue() ? size.ParsedValue : 5) * 1024,
+                EnablePartitioning = partitioning.HasValue()
+            };
+
+            return client.CreateQueueAsync(queueDescription);
         }
     }
 }
