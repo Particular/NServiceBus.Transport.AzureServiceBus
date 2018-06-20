c55e7091bcb676fae0aed07cb4873dcd95063e46
Sean Feldman <feldman.sean@gmail.com>
2018-06-20 13:44:49 -0600

    Add workaround to construct stand-alone generic command options
    Co-authored-by: Brandon Ording <bording@gmail.com>
    Co-authored-by: Sean Feldman <feldman.sean@gmail.com>

---
+++ src/CommandLine/Workaround.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Nate McMaster.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Globalization;
+
+namespace McMaster.Extensions.CommandLineUtils.Abstractions
+{
+    // TODO: Remove when https://github.com/natemcmaster/CommandLineUtils/issues/109 is fixed
+    internal class Int32ValueParser : IValueParser<int>
+    {
+        private Int32ValueParser()
+        { }
+
+        public static Int32ValueParser Singleton { get; } = new Int32ValueParser();
+
+        public Type TargetType { get; } = typeof(int);
+
+        public int Parse(string argName, string value, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return default;
+            }
+
+            if (!int.TryParse(value, NumberStyles.Integer, culture.NumberFormat, out var result))
+            {
+                throw new FormatException($"Invalid value specified for {argName}. '{value}' is not a valid number.");
+            }
+
+            return result;
+        }
+
+        object IValueParser.Parse(string argName, string value, CultureInfo culture)
+            => this.Parse(argName, value, culture);
+    }
+}
