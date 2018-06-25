2618649f831cf6f1c005bced66049c08feb277f6
Sean Feldman <feldman.sean@gmail.com>
2018-06-25 15:40:57 -0600

    Initial implementation of TransportTests
    Co-authored-by: Brandon Ording <bording@gmail.com>
    Co-authored-by: Sean Feldman <feldman.sean@gmail.com>

---
+++ src/TransportTests/ConfigureAzureServiceBusTransportInfrastructure.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Settings;
+using NServiceBus.TransportTests;
+
+public class ConfigureAzureServiceBusTransportInfrastructure : IConfigureTransportInfrastructure
+{
+    public TransportConfigurationResult Configure(SettingsHolder settings, TransportTransactionMode transactionMode)
+    {
+        //TODO: fails on settings.LocalAddress()
+        var result = new TransportConfigurationResult();
+
+        result.PurgeInputQueueOnStartup = false;
+
+        var transport = new AzureServiceBusTransport();
+        var connectionString = Environment.GetEnvironmentVariable("AzureServiceBus_ConnectionString");
+        result.TransportInfrastructure = transport.Initialize(settings, connectionString);
+
+        return result;
+    }
+
+    public Task Cleanup()
+    {
+        return Task.CompletedTask;
+    }
+}
