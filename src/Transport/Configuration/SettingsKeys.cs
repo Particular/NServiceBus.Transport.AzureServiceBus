e91b34200e2061712ab7b4491c2337ba29f7dbea
Sean Feldman <feldman.sean@gmail.com>
2018-06-25 14:47:05 -0600

    Initial settings stub
    Co-authored-by: Brandon Ording <bording@gmail.com>
    Co-authored-by: Sean Feldman <feldman.sean@gmail.com>

---
+++ src/Transport/Configuration/SettingsKeys.cs
@@ -0,0 +1,16 @@
+﻿namespace NServiceBus.Transport.AzureServiceBus
+{
+    static class SettingsKeys
+    {
+        const string Base = "AzureServiceBus.";
+        public const string TopicName = Base + nameof(TopicName);
+        public const string MaximumSizeInGB = Base + nameof(MaximumSizeInGB);
+        public const string EnablePartitioning = Base + nameof(EnablePartitioning);
+        public const string PrefetchMultiplier = Base + nameof(PrefetchMultiplier);
+        public const string PrefetchOverride = Base + nameof(PrefetchOverride);
+        public const string TimeToWaitBeforeTriggeringCircuitBreaker = Base + nameof(TimeToWaitBeforeTriggeringCircuitBreaker);
+        public const string SubscriptionNameShortener = Base + nameof(SubscriptionNameShortener);
+        public const string RuleNameShortener = Base + nameof(RuleNameShortener);
+        public const string UseWebSockets = Base + nameof(UseWebSockets);
+    }
+}
