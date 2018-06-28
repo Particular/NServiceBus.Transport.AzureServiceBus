d3a6d79824058149310169d9c0cbbe29d9a122db
Brandon Ording <bording@gmail.com>
2018-06-28 11:50:41 -0400

    Add API approval test

---
+++ src/Tests/APIApprovals.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+using NServiceBus;
+using NServiceBus.Transport.AzureServiceBus.Tests;
+using NUnit.Framework;
+using PublicApiGenerator;
+
+[TestFixture]
+public class APIApprovals
+{
+    [Test]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void Approve()
+    {
+        var publicApi = ApiGenerator.GeneratePublicApi(typeof(AzureServiceBusTransport).Assembly, excludeAttributes: new[] { "System.Runtime.Versioning.TargetFrameworkAttribute" });
+        TestApprover.Verify(publicApi);
+    }
+}
---
+++ src/Tests/TestApprover.cs
@@ -0,0 +1,22 @@
+﻿namespace NServiceBus.Transport.AzureServiceBus.Tests
+{
+    using System.IO;
+    using ApprovalTests;
+    using ApprovalTests.Namers;
+    using NUnit.Framework;
+
+    static class TestApprover
+    {
+        public static void Verify(string text)
+        {
+            var writer = new ApprovalTextWriter(text);
+            var namer = new ApprovalNamer();
+            Approvals.Verify(writer, namer, Approvals.GetReporter());
+        }
+
+        class ApprovalNamer : UnitTestFrameworkNamer
+        {
+            public override string SourcePath { get; } = Path.Combine(TestContext.CurrentContext.TestDirectory, "..", "..", "..", "ApprovalFiles");
+        }
+    }
+}
