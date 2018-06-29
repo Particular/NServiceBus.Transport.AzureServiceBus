Particular/NServiceBus.Transport.AzureServiceBus

commit c6e6a8ebab7408f47b53c797ea9b53ae44394ca3

Use receiver parts for MessageSenderPool as tuple
Co-authored-by: Brandon Ording <bording@gmail.com>
Co-authored-by: Sean Feldman <feldman.sean@gmail.com>

src/Transport/Sending/MessageDispatcher.cs
@@ -21,8 +21,7 @@
         {
             // Assumption: we're not implementing batching as it will be done by ASB client
 
-            transaction.TryGet<ServiceBusConnection>(out var connection);
-            transaction.TryGet<string>("IncomingQueue", out var incomingQueue);
+            transaction.TryGet<(ServiceBusConnection, string)>(out var receiverConnectionAndPath);
             transaction.TryGet<string>("IncomingQueue.PartitionKey", out var partitionKey);
 
             var unicastTransportOperations = outgoingMessages.UnicastTransportOperations;
@@ -32,10 +31,9 @@
 
             foreach (var transportOperation in unicastTransportOperations)
             {
-                var connectionToUse = transportOperation.RequiredDispatchConsistency == DispatchConsistency.Isolated ? null : connection;
-                var incomingQueueToUse = transportOperation.RequiredDispatchConsistency == DispatchConsistency.Isolated ? null : incomingQueue;
+                var receiverConnectionAndPathToUse = transportOperation.RequiredDispatchConsistency == DispatchConsistency.Isolated ? receiverConnectionAndPath : (null, null);
 
-                var sender = messageSenderPool.GetMessageSender(transportOperation.Destination, connectionToUse, incomingQueueToUse);
+                var sender = messageSenderPool.GetMessageSender(transportOperation.Destination, receiverConnectionAndPathToUse);
 
                 try
                 {
@@ -56,10 +54,9 @@
 
             foreach (var transportOperation in multicastTransportOperations)
             {
-                var connectionToUse = transportOperation.RequiredDispatchConsistency == DispatchConsistency.Isolated ? null : connection;
-                var incomingQueueToUse = transportOperation.RequiredDispatchConsistency == DispatchConsistency.Isolated ? null : incomingQueue;
+                var receiverConnectionAndPathToUse = transportOperation.RequiredDispatchConsistency == DispatchConsistency.Isolated ? receiverConnectionAndPath : (null, null);
 
-                var sender = messageSenderPool.GetMessageSender(topicName, connectionToUse, incomingQueueToUse);
+                var sender = messageSenderPool.GetMessageSender(topicName, receiverConnectionAndPathToUse);
 
                 try
                 {

src/Tests/Sending/MessageSenderPoolTests.cs
@@ -11,11 +11,11 @@
         {
             var pool = new MessageSenderPool(new ServiceBusConnectionStringBuilder("Endpoint=sb://fake.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=fake="), null);
 
-            var firstMessageSender = pool.GetMessageSender("dest", null, null);
+            var firstMessageSender = pool.GetMessageSender("dest", (null, null));
 
             pool.ReturnMessageSender(firstMessageSender);
 
-            var secondMessageSender = pool.GetMessageSender("dest", null, null);
+            var secondMessageSender = pool.GetMessageSender("dest", (null, null));
 
             Assert.AreEqual(firstMessageSender, secondMessageSender);
         }

src/Transport/Receiving/MessagePump.cs
@@ -258,8 +258,7 @@
 
             if (pushSettings.RequiredTransactionMode == TransportTransactionMode.SendsAtomicWithReceive)
             {
-                transportTransaction.Set(receiver.ServiceBusConnection);
-                transportTransaction.Set("IncomingQueue", pushSettings.InputQueue);
+                transportTransaction.Set((receiver.ServiceBusConnection, receiver.Path));
                 transportTransaction.Set("IncomingQueue.PartitionKey", incomingQueuePartitionKey);
             }
 

src/Transport/Sending/MessageSenderPool.cs
@@ -14,19 +14,20 @@
             this.connectionStringBuilder = connectionStringBuilder;
             this.tokenProvider = tokenProvider;
 
-            senders = new ConcurrentDictionary<(string, ServiceBusConnection, string), ConcurrentQueue<MessageSender>>();
+            senders = new ConcurrentDictionary<(string, (ServiceBusConnection, string)), ConcurrentQueue<MessageSender>>();
         }
 
-        public MessageSender GetMessageSender(string destination, ServiceBusConnection connection, string incomingQueue)
+        public MessageSender GetMessageSender(string destination, (ServiceBusConnection connection, string path) receiverConnectionAndPath)
         {
-            var sendersForDestination = senders.GetOrAdd((destination, connection, incomingQueue), tuple => new ConcurrentQueue<MessageSender>());
+            var sendersForDestination = senders.GetOrAdd((destination, receiverConnectionAndPath), _ => new ConcurrentQueue<MessageSender>());
 
             if (!sendersForDestination.TryDequeue(out var sender) || sender.IsClosedOrClosing)
             {
                 // Send-Via case
-                if (connection != null)
+                // TODO: replace with "if (receiverConnectionAndPath != (null, null))" when Resharper 2018.2 is out to support C# 7.3 syntax
+                if (receiverConnectionAndPath.path != null)
                 {
-                    sender = new MessageSender(connection, destination, incomingQueue);
+                    sender = new MessageSender(receiverConnectionAndPath.connection, destination, receiverConnectionAndPath.path);
                 }
                 else
                 {
@@ -53,7 +54,7 @@
 
             var connectionToUse = sender.OwnsConnection ? null : sender.ServiceBusConnection;
 
-            if (senders.TryGetValue((sender.Path, connectionToUse, sender.TransferDestinationPath), out var sendersForDestination))
+            if (senders.TryGetValue((sender.Path, (connectionToUse, sender.TransferDestinationPath)), out var sendersForDestination))
             {
                 sendersForDestination.Enqueue(sender);
             }
@@ -79,6 +80,6 @@
         readonly ServiceBusConnectionStringBuilder connectionStringBuilder;
         readonly ITokenProvider tokenProvider;
 
-        ConcurrentDictionary<(string destination, ServiceBusConnection connnection, string incomingQueue), ConcurrentQueue<MessageSender>> senders;
+        ConcurrentDictionary<(string destination, (ServiceBusConnection connnection, string incomingQueue)), ConcurrentQueue<MessageSender>> senders;
     }
 }

commit af5bf5812f9ec1f1685d8dd2de7e1ad1ed7d9986

Fix regression introduced by code refactoring in c6e6a8ebab7408f47b53c797ea9b53ae44394ca3
Co-authored-by: Brandon Ording <bording@gmail.com>
Co-authored-by: Sean Feldman <feldman.sean@gmail.com>

src/Transport/Sending/MessageDispatcher.cs
@@ -31,7 +31,7 @@
 
             foreach (var transportOperation in unicastTransportOperations)
             {
-                var receiverConnectionAndPathToUse = transportOperation.RequiredDispatchConsistency == DispatchConsistency.Isolated ? receiverConnectionAndPath : (null, null);
+                var receiverConnectionAndPathToUse = transportOperation.RequiredDispatchConsistency == DispatchConsistency.Isolated ? (null, null) : receiverConnectionAndPath;
 
                 var sender = messageSenderPool.GetMessageSender(transportOperation.Destination, receiverConnectionAndPathToUse);
 
@@ -54,7 +54,7 @@
 
             foreach (var transportOperation in multicastTransportOperations)
             {
-                var receiverConnectionAndPathToUse = transportOperation.RequiredDispatchConsistency == DispatchConsistency.Isolated ? receiverConnectionAndPath : (null, null);
+                var receiverConnectionAndPathToUse = transportOperation.RequiredDispatchConsistency == DispatchConsistency.Isolated ? (null, null) : receiverConnectionAndPath;
 
                 var sender = messageSenderPool.GetMessageSender(topicName, receiverConnectionAndPathToUse);
 