44b46423d3e53e425e4fd837e01eb9ecebb71ef9
Sean Feldman <feldman.sean@gmail.com>
2018-08-20 15:13:24 -0600

    Use Particular.Approvals for API approval
    Co-authored-by: Brandon Ording <bording@gmail.com>
    Co-authored-by: Sean Feldman <feldman.sean@gmail.com>

--- src/Tests/APIApprovals.cs
+++ src/Tests/APIApprovals.cs
@@ -1,17 +1,15 @@
-﻿using System.Runtime.CompilerServices;
-using NServiceBus;
-using NServiceBus.Transport.AzureServiceBus.Tests;
+﻿using NServiceBus;
 using NUnit.Framework;
+using Particular.Approvals;
 using PublicApiGenerator;
 
 [TestFixture]
 public class APIApprovals
 {
     [Test]
-    [MethodImpl(MethodImplOptions.NoInlining)]
     public void Approve()
     {
         var publicApi = ApiGenerator.GeneratePublicApi(typeof(AzureServiceBusTransport).Assembly, excludeAttributes: new[] { "System.Runtime.Versioning.TargetFrameworkAttribute" });
-        TestApprover.Verify(publicApi);
+        Approver.Verify(publicApi);
     }
 }
