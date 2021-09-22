dc482a55b85928464ee8b3bcb90c380a18f5860e
Mauro Servienti <mauro.servienti@gmail.com>
2021-09-22 12:01:01 +0200

    Drop not anymore needed pooler tests

--- src/Tests/Sending/MessageSenderPoolTests.cs
+++ src/Tests/Sending/MessageSenderPoolTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace NServiceBus.Transport.AzureServiceBus.Tests.Sending
 {
     using System.Threading.Tasks;
+    using Azure.Messaging.ServiceBus;
     using Microsoft.Azure.ServiceBus;
     using NUnit.Framework;
 
@@ -10,18 +11,18 @@
         [Test]
         public async Task Should_get_cached_sender_per_destination()
         {
-            var pool = new MessageSenderPool(new ServiceBusConnectionStringBuilder(connectionString), null, null);
+            var pool = new MessageSenderPool(new ServiceBusClient(connectionString));
 
             try
             {
-                var firstMessageSenderDest1 = pool.GetMessageSender("dest1", (null, null));
+                var firstMessageSenderDest1 = pool.GetMessageSender("dest1");
                 pool.ReturnMessageSender(firstMessageSenderDest1);
 
-                var firstMessageSenderDest2 = pool.GetMessageSender("dest2", (null, null));
+                var firstMessageSenderDest2 = pool.GetMessageSender("dest2");
                 pool.ReturnMessageSender(firstMessageSenderDest2);
 
-                var secondMessageSenderDest1 = pool.GetMessageSender("dest1", (null, null));
-                var secondMessageSenderDest2 = pool.GetMessageSender("dest2", (null, null));
+                var secondMessageSenderDest1 = pool.GetMessageSender("dest1");
+                var secondMessageSenderDest2 = pool.GetMessageSender("dest2");
 
                 Assert.AreSame(firstMessageSenderDest1, secondMessageSenderDest1);
                 Assert.AreSame(firstMessageSenderDest2, secondMessageSenderDest2);
@@ -31,64 +32,6 @@
             finally
             {
                 await pool.Close();
-            }
-        }
-
-        [Test]
-        public async Task Should_return_correctly_configured_sender()
-        {
-            var pool = new MessageSenderPool(new ServiceBusConnectionStringBuilder(connectionString), null, null);
-            var connection = new ServiceBusConnection(connectionString);
-
-            try
-            {
-                // unfortunately it is not possible to assert the token provider
-                var nonSendViaSender = pool.GetMessageSender("dest1", (null, null));
-                var sendViaSender = pool.GetMessageSender("dest2", (connection, "via"));
-
-                Assert.AreEqual("dest1", nonSendViaSender.Path);
-                Assert.IsNull(nonSendViaSender.TransferDestinationPath);
-                Assert.IsNull(nonSendViaSender.ViaEntityPath);
-                Assert.IsTrue(nonSendViaSender.OwnsConnection);
-
-                Assert.AreEqual("via", sendViaSender.ViaEntityPath);
-                Assert.AreEqual("via", sendViaSender.Path);
-                Assert.AreEqual("dest2", sendViaSender.TransferDestinationPath);
-                Assert.IsFalse(sendViaSender.OwnsConnection);
-            }
-            finally
-            {
-                await pool.Close();
-                await connection.CloseAsync();
-            }
-        }
-
-        [Test]
-        public async Task Should_get_cached_sender_per_destination_for_send_via()
-        {
-            var pool = new MessageSenderPool(new ServiceBusConnectionStringBuilder(connectionString), null, null);
-            var connection = new ServiceBusConnection(connectionString);
-
-            try
-            {
-                var firstMessageSenderDest1 = pool.GetMessageSender("dest1", (connection, "via"));
-                pool.ReturnMessageSender(firstMessageSenderDest1);
-
-                var firstMessageSenderDest2 = pool.GetMessageSender("dest2", (connection, "via"));
-                pool.ReturnMessageSender(firstMessageSenderDest2);
-
-                var secondMessageSenderDest1 = pool.GetMessageSender("dest1", (connection, "via"));
-                var secondMessageSenderDest2 = pool.GetMessageSender("dest2", (connection, "via"));
-
-                Assert.AreSame(firstMessageSenderDest1, secondMessageSenderDest1);
-                Assert.AreSame(firstMessageSenderDest2, secondMessageSenderDest2);
-                Assert.AreNotSame(firstMessageSenderDest1, firstMessageSenderDest2);
-                Assert.AreNotSame(secondMessageSenderDest1, secondMessageSenderDest2);
-            }
-            finally
-            {
-                await pool.Close();
-                await connection.CloseAsync();
             }
         }
 
