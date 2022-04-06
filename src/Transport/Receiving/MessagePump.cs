0e288a6715beda5f8474a87dfb5736929715f318
Ramon Smits <ramon.smits@gmail.com>
2022-04-06 20:59:11 +0200

    Changed to add ServiceBusReceiver
    For renewal the only stable method is to use the receiver used to invoke `ReceiveMessageAsync ` to also invoke `RenewMessageLockAsync`. Creating a temporary ServiceBusReceiver via `ServiceBusClient` stored in the `TransportTransaction` is not stable. Any message processing taking longer then 10 minutes (likely http timeout related) can cause the receiver to drop its connection.

--- src/Transport/Receiving/MessagePump.cs
+++ src/Transport/Receiving/MessagePump.cs
@@ -290,7 +290,7 @@
                     var transportTransaction = CreateTransportTransaction(message.PartitionKey, transaction);
 
                     contextBag.Set(message);
-                    contextBag.Set(serviceBusClient);
+                    contextBag.Set(receiver);
 
                     var messageContext = new MessageContext(messageId, headers, body, transportTransaction, ReceiveAddress, contextBag);
 
