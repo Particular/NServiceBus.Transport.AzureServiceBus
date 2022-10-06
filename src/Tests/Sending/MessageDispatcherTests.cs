3d923e458daf1beaa979d275dc3789927420be01
danielmarbach <daniel.marbach@openplace.net>
2022-10-06 16:15:17 +0200

    Add more tests

--- src/Tests/Sending/MessageDispatcherTests.cs
+++ src/Tests/Sending/MessageDispatcherTests.cs
@@ -3,10 +3,7 @@ namespace NServiceBus.Transport.AzureServiceBus.Tests.Sending
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Runtime.CompilerServices;
-    using System.Threading;
     using System.Threading.Tasks;
-    using Azure.Messaging.ServiceBus;
     using NUnit.Framework;
     using Routing;
 
@@ -39,6 +36,37 @@ namespace NServiceBus.Transport.AzureServiceBus.Tests.Sending
             await dispatcher.Dispatch(new TransportOperations(operation1, operation2), new TransportTransaction());
 
             var sender = client.Senders["SomeDestination"];
+
+            Assert.That(sender.IndividuallySentMessages, Has.Count.EqualTo(2));
+            Assert.That(sender.BatchSentMessages, Is.Empty);
+        }
+
+        [Test]
+        public async Task Should_dispatch_multicast_isolated_dispatches_individually()
+        {
+            var client = new FakeServiceBusClient();
+
+            var dispatcher = new MessageDispatcher(new MessageSenderRegistry(client), "sometopic");
+
+            var operation1 =
+                new TransportOperation(new OutgoingMessage("SomeId",
+                        new Dictionary<string, string>(),
+                        ReadOnlyMemory<byte>.Empty),
+                    new MulticastAddressTag(typeof(SomeEvent)),
+                    new DispatchProperties(),
+                    DispatchConsistency.Isolated);
+
+            var operation2 =
+                new TransportOperation(new OutgoingMessage("SomeOtherId",
+                        new Dictionary<string, string>(),
+                        ReadOnlyMemory<byte>.Empty),
+                    new MulticastAddressTag(typeof(SomeEvent)),
+                    new DispatchProperties(),
+                    DispatchConsistency.Isolated);
+
+            await dispatcher.Dispatch(new TransportOperations(operation1, operation2), new TransportTransaction());
+
+            var sender = client.Senders["sometopic"];
 
             Assert.That(sender.IndividuallySentMessages, Has.Count.EqualTo(2));
             Assert.That(sender.BatchSentMessages, Is.Empty);
@@ -79,6 +107,37 @@ namespace NServiceBus.Transport.AzureServiceBus.Tests.Sending
         }
 
         [Test]
+        public async Task Should_dispatch_multicast_isolated_dispatches_individually_regardless_of_the_event_to_the_topic()
+        {
+            var client = new FakeServiceBusClient();
+
+            var dispatcher = new MessageDispatcher(new MessageSenderRegistry(client), "sometopic");
+
+            var operation1 =
+                new TransportOperation(new OutgoingMessage("SomeId",
+                        new Dictionary<string, string>(),
+                        ReadOnlyMemory<byte>.Empty),
+                    new MulticastAddressTag(typeof(SomeEvent)),
+                    new DispatchProperties(),
+                    DispatchConsistency.Isolated);
+
+            var operation2 =
+                new TransportOperation(new OutgoingMessage("SomeOtherId",
+                        new Dictionary<string, string>(),
+                        ReadOnlyMemory<byte>.Empty),
+                    new MulticastAddressTag(typeof(SomeOtherEvent)),
+                    new DispatchProperties(),
+                    DispatchConsistency.Isolated);
+
+            await dispatcher.Dispatch(new TransportOperations(operation1, operation2), new TransportTransaction());
+
+            var sender = client.Senders["sometopic"];
+
+            Assert.That(sender.IndividuallySentMessages, Has.Count.EqualTo(2));
+            Assert.That(sender.BatchSentMessages, Is.Empty);
+        }
+
+        [Test]
         public async Task Should_dispatch_unicast_default_dispatches_together_as_batch()
         {
             var client = new FakeServiceBusClient();
@@ -104,6 +163,39 @@ namespace NServiceBus.Transport.AzureServiceBus.Tests.Sending
             await dispatcher.Dispatch(new TransportOperations(operation1, operation2), new TransportTransaction());
 
             var sender = client.Senders["SomeDestination"];
+
+            Assert.That(sender.IndividuallySentMessages, Is.Empty);
+            Assert.That(sender.BatchSentMessages, Has.Count.EqualTo(1));
+            var batchContent = sender[sender.BatchSentMessages.ElementAt(0)];
+            Assert.That(batchContent, Has.Count.EqualTo(2));
+        }
+
+        [Test]
+        public async Task Should_dispatch_multicast_default_dispatches_together_as_batch()
+        {
+            var client = new FakeServiceBusClient();
+
+            var dispatcher = new MessageDispatcher(new MessageSenderRegistry(client), "sometopic");
+
+            var operation1 =
+                new TransportOperation(new OutgoingMessage("SomeId",
+                        new Dictionary<string, string>(),
+                        ReadOnlyMemory<byte>.Empty),
+                    new MulticastAddressTag(typeof(SomeEvent)),
+                    new DispatchProperties(),
+                    DispatchConsistency.Default);
+
+            var operation2 =
+                new TransportOperation(new OutgoingMessage("SomeOtherId",
+                        new Dictionary<string, string>(),
+                        ReadOnlyMemory<byte>.Empty),
+                    new MulticastAddressTag(typeof(SomeEvent)),
+                    new DispatchProperties(),
+                    DispatchConsistency.Default);
+
+            await dispatcher.Dispatch(new TransportOperations(operation1, operation2), new TransportTransaction());
+
+            var sender = client.Senders["sometopic"];
 
             Assert.That(sender.IndividuallySentMessages, Is.Empty);
             Assert.That(sender.BatchSentMessages, Has.Count.EqualTo(1));
@@ -149,61 +241,100 @@ namespace NServiceBus.Transport.AzureServiceBus.Tests.Sending
             var someOtherDestinationBatchContent = someOtherDestinationSender[someOtherDestinationSender.BatchSentMessages.ElementAt(0)];
             Assert.That(someOtherDestinationBatchContent, Has.Count.EqualTo(1));
         }
-    }
 
-    public class FakeSender : ServiceBusSender
-    {
-        readonly List<ServiceBusMessage> sentMessages = new List<ServiceBusMessage>();
-        readonly List<ServiceBusMessageBatch> batchedMessages = new List<ServiceBusMessageBatch>();
-        readonly ConditionalWeakTable<ServiceBusMessageBatch, IReadOnlyCollection<ServiceBusMessage>>
-            batchToBackingStore =
-                new ConditionalWeakTable<ServiceBusMessageBatch, IReadOnlyCollection<ServiceBusMessage>>();
-
-        public IReadOnlyCollection<ServiceBusMessage> IndividuallySentMessages => sentMessages;
-        public IReadOnlyCollection<ServiceBusMessageBatch> BatchSentMessages => batchedMessages;
-
-        public IReadOnlyCollection<ServiceBusMessage> this[ServiceBusMessageBatch batch]
+        [Test]
+        public async Task Should_dispatch_multicast_default_dispatches_together_as_batch_regardless_of_the_event_to_the_topic()
         {
-            get => batchToBackingStore.TryGetValue(batch, out var store) ? store : Array.Empty<ServiceBusMessage>();
-            set => throw new NotSupportedException();
+            var client = new FakeServiceBusClient();
+
+            var dispatcher = new MessageDispatcher(new MessageSenderRegistry(client), "sometopic");
+
+            var operation1 =
+                new TransportOperation(new OutgoingMessage("SomeId",
+                        new Dictionary<string, string>(),
+                        ReadOnlyMemory<byte>.Empty),
+                    new MulticastAddressTag(typeof(SomeEvent)),
+                    new DispatchProperties(),
+                    DispatchConsistency.Default);
+
+            var operation2 =
+                new TransportOperation(new OutgoingMessage("SomeOtherId",
+                        new Dictionary<string, string>(),
+                        ReadOnlyMemory<byte>.Empty),
+                    new MulticastAddressTag(typeof(SomeOtherEvent)),
+                    new DispatchProperties(),
+                    DispatchConsistency.Default);
+
+            await dispatcher.Dispatch(new TransportOperations(operation1, operation2), new TransportTransaction());
+
+            var sender = client.Senders["sometopic"];
+
+            Assert.That(sender.IndividuallySentMessages, Is.Empty);
+            Assert.That(sender.BatchSentMessages, Has.Count.EqualTo(1));
+            var batchContent = sender[sender.BatchSentMessages.ElementAt(0)];
+            Assert.That(batchContent, Has.Count.EqualTo(2));
         }
 
-        public override ValueTask<ServiceBusMessageBatch> CreateMessageBatchAsync(CancellationToken cancellationToken = default)
+        [Test]
+        public async Task Should_allow_mixing_operations()
         {
-            var batchMessageStore = new List<ServiceBusMessage>();
-            ServiceBusMessageBatch serviceBusMessageBatch = ServiceBusModelFactory.ServiceBusMessageBatch(256 * 1024, batchMessageStore);
-            batchToBackingStore.Add(serviceBusMessageBatch, batchMessageStore);
-            return new ValueTask<ServiceBusMessageBatch>(
-                serviceBusMessageBatch);
+            var client = new FakeServiceBusClient();
+
+            var dispatcher = new MessageDispatcher(new MessageSenderRegistry(client), "sometopic");
+
+            var operation1 =
+                new TransportOperation(new OutgoingMessage("Operation1",
+                        new Dictionary<string, string>(),
+                        ReadOnlyMemory<byte>.Empty),
+                    new UnicastAddressTag("SomeDestination"),
+                    new DispatchProperties(),
+                    DispatchConsistency.Isolated);
+
+            var operation2 =
+                new TransportOperation(new OutgoingMessage("Operation2",
+                        new Dictionary<string, string>(),
+                        ReadOnlyMemory<byte>.Empty),
+                    new MulticastAddressTag(typeof(SomeEvent)),
+                    new DispatchProperties(),
+                    DispatchConsistency.Isolated);
+
+            var operation3 =
+                new TransportOperation(new OutgoingMessage("Operation3",
+                        new Dictionary<string, string>(),
+                        ReadOnlyMemory<byte>.Empty),
+                    new UnicastAddressTag("SomeOtherDestination"),
+                    new DispatchProperties(),
+                    DispatchConsistency.Default);
+
+            var operation4 =
+                new TransportOperation(new OutgoingMessage("Operation4",
+                        new Dictionary<string, string>(),
+                        ReadOnlyMemory<byte>.Empty),
+                    new MulticastAddressTag(typeof(SomeOtherEvent)),
+                    new DispatchProperties(),
+                    DispatchConsistency.Default);
+
+            await dispatcher.Dispatch(new TransportOperations(operation1, operation2, operation3, operation4), new TransportTransaction());
+
+            var someDestinationSender = client.Senders["SomeDestination"];
+            var someOtherDestinationSender = client.Senders["SomeOtherDestination"];
+            var someTopicSender = client.Senders["sometopic"];
+
+            Assert.That(someDestinationSender.IndividuallySentMessages, Has.Count.EqualTo(1));
+            Assert.That(someDestinationSender.BatchSentMessages, Is.Empty);
+
+            Assert.That(someOtherDestinationSender.IndividuallySentMessages, Is.Empty);
+            Assert.That(someOtherDestinationSender.BatchSentMessages, Has.Count.EqualTo(1));
+            var someOtherDestinationBatchContent = someOtherDestinationSender[someOtherDestinationSender.BatchSentMessages.ElementAt(0)];
+            Assert.That(someOtherDestinationBatchContent, Has.Count.EqualTo(1));
+
+            Assert.That(someTopicSender.IndividuallySentMessages, Has.Count.EqualTo(1));
+            Assert.That(someTopicSender.BatchSentMessages, Has.Count.EqualTo(1));
+            var someTopicSenderBatchContent = someTopicSender[someTopicSender.BatchSentMessages.ElementAt(0)];
+            Assert.That(someTopicSenderBatchContent, Has.Count.EqualTo(1));
         }
 
-        public override Task SendMessageAsync(ServiceBusMessage message, CancellationToken cancellationToken = default)
-        {
-            cancellationToken.ThrowIfCancellationRequested();
-            sentMessages.Add(message);
-            return Task.CompletedTask;
-        }
-
-        public override Task SendMessagesAsync(ServiceBusMessageBatch messageBatch,
-            CancellationToken cancellationToken = new CancellationToken())
-        {
-            cancellationToken.ThrowIfCancellationRequested();
-            batchedMessages.Add(messageBatch);
-            return Task.CompletedTask;
-        }
-    }
-
-    public class FakeServiceBusClient : ServiceBusClient
-    {
-        public Dictionary<string, FakeSender> Senders { get; } = new Dictionary<string, FakeSender>();
-
-        public override ServiceBusSender CreateSender(string queueOrTopicName)
-        {
-            if (!Senders.ContainsKey(queueOrTopicName))
-            {
-                Senders[queueOrTopicName] = new FakeSender();
-            }
-            return Senders[queueOrTopicName];
-        }
+        class SomeEvent { }
+        class SomeOtherEvent { }
     }
 }
