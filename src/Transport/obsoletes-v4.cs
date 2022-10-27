577cdaf2caa7e0c6d56badd021c6d12cd759e9b1
danielmarbach <daniel.marbach@openplace.net>
2022-10-27 17:40:42 +0200

    Make things ready for more obsoletions

---
+++ src/Transport/obsoletes-v4.cs
@@ -0,0 +1,3 @@
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
