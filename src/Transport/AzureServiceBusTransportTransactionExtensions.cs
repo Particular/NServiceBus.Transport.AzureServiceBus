b53b47873f4295dbf1a958dcf2fbaddca2911ccb
Daniel Marbach <daniel.marbach@openplace.net>
2022-11-02 15:17:06 +0100

    Merge pull request #667 from Particular/ability-to-scope
    Introduce ToTransactionScope as an extension on AzureServiceBusTransportTransaction

---
+++ src/Transport/AzureServiceBusTransportTransactionExtensions.cs
@@ -0,0 +1,18 @@
+namespace NServiceBus.Transport.AzureServiceBus
+{
+    using System.Transactions;
+
+    /// <summary>
+    /// Provides extension methods for <see cref="AzureServiceBusTransportTransaction"/>
+    /// </summary>
+    public static class AzureServiceBusTransportTransactionExtensions
+    {
+        /// <summary>
+        /// Returns a new scope that takes into account the internally managed transaction by either
+        /// providing the transaction to the scope or creating a suppress scope.
+        /// </summary>
+        public static TransactionScope ToTransactionScope(
+            this AzureServiceBusTransportTransaction azureServiceBusTransaction) =>
+            azureServiceBusTransaction.Transaction.ToScope();
+    }
+}
