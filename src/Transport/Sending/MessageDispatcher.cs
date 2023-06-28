282b19c889fa1c8a19f088ace3dd3088243ba84b
Mike Minutillo <mike.minutillo@particular.net>
2023-06-28 16:38:43 +0800

    Collapse into dispatches into a single loop

--- src/Transport/Sending/MessageDispatcher.cs
+++ src/Transport/Sending/MessageDispatcher.cs
@@ -27,23 +27,15 @@
             var unicastTransportOperations = outgoingMessages.UnicastTransportOperations;
             var multicastTransportOperations = outgoingMessages.MulticastTransportOperations;
 
+            var transportOperations =
+                new List<IOutgoingTransportOperation>(unicastTransportOperations.Count +
+                                                      multicastTransportOperations.Count);
+            transportOperations.AddRange(unicastTransportOperations);
+            transportOperations.AddRange(multicastTransportOperations);
+
             var tasks = new List<Task>(unicastTransportOperations.Count + multicastTransportOperations.Count);
 
-            foreach (var transportOperation in unicastTransportOperations)
-            {
-                var destination = transportOperation.ExtractDestination(defaultMulticastRoute: topicName);
-
-                var sender = messageSenderRegistry.GetMessageSender(destination, serviceBusClient);
-
-                var message = transportOperation.Message.ToAzureServiceBusMessage(transportOperation.DeliveryConstraints, partitionKey);
-
-                ApplyCustomizationToOutgoingNativeMessage(context, transportOperation, message);
-
-                var transactionToUse = transportOperation.RequiredDispatchConsistency == DispatchConsistency.Isolated ? null : committableTransaction;
-                tasks.Add(DispatchOperation(sender, message, transactionToUse));
-            }
-
-            foreach (var transportOperation in multicastTransportOperations)
+            foreach (var transportOperation in transportOperations)
             {
                 var destination = transportOperation.ExtractDestination(defaultMulticastRoute: topicName);
 
