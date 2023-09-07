40d534cb14a1b437cc07e383d4f4602eb4f868a4
Andreas Öhlund <andreas.ohlund@particular.net>
2023-09-07 16:26:41 +0200

    Remove uneeded override (#874)

--- src/Transport/obsoletes-v4.cs
+++ src/Transport/obsoletes-v4.cs
@@ -3,17 +3,9 @@
 namespace NServiceBus
 {
     using System;
-    using NServiceBus.Transport;
 
     public partial class AzureServiceBusTransport
     {
-        [ObsoleteEx(Message = "Inject the ITransportAddressResolver type to access the address translation mechanism at runtime. See the NServiceBus version 8 upgrade guide for further details.",
-                    TreatAsErrorFromVersion = "4",
-                    RemoveInVersion = "5")]
-#pragma warning disable CS0672 // Member overrides obsolete member
-        public override string ToTransportAddress(QueueAddress address) => throw new NotImplementedException();
-#pragma warning restore CS0672 // Member overrides obsolete member
-
         [ObsoleteEx(Message = "It is possible to represent the publish and subscribe topic separately by specifying a topology.",
             TreatAsErrorFromVersion = "4",
             RemoveInVersion = "5",
