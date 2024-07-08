c0797b5a01b1ce0b2ab0431be2b211a6b99c1640
Ramon Smits <ramon.smits@gmail.com>
2024-07-08 14:18:39 +0200

    🛠️ Moved Obsolete attribute to prevent compiler warning

--- src/Transport/Sending/TransportOperationEx.cs
+++ src/Transport/Sending/TransportOperationEx.cs
@@ -2,11 +2,19 @@ namespace NServiceBus.Transport.AzureServiceBus.Experimental;
 
 using System;
 
-[Obsolete("Experimental")]
+/// <summary>
+/// TransportOperation extensions
+/// </summary>
 public static class TransportOperationExt
 {
     internal const string DisableLegacyHeadersKey = "DisableLegacyHeaders";
 
+    /// <summary>
+    /// Disable adding legacy NServiceBus headers for backwards compatibility with NServiceBus.AzureServiceBus (ASBL)
+    /// </summary>
+    /// <param name="instance"></param>
+    [Obsolete("This is experimental")]
+    [DoNotWarnAboutObsoleteUsage]
     public static void DisableLegacyHeaders(this TransportOperation instance)
     {
         instance.Properties.Add(DisableLegacyHeadersKey, bool.TrueString);
