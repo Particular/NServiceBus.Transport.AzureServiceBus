203983d37152b93ab8803222475330d521688588
Daniel Marbach <daniel.marbach@openplace.net>
2024-09-24 01:55:05 +0200

    Track messages that successfully completed the message or error pipeline but failed to get acknowledged due to expired leases in receiveonly mode (#1034)
    # Conflicts:
    #	src/AcceptanceTests/NServiceBus.Transport.AzureServiceBus.AcceptanceTests.csproj
    #	src/CommandLine/NServiceBus.Transport.AzureServiceBus.CommandLine.csproj
    #	src/CommandLineTests/NServiceBus.Transport.AzureServiceBus.CommandLine.Tests.csproj
    #	src/Tests/FakeReceiver.cs
    #	src/Tests/NServiceBus.Transport.AzureServiceBus.Tests.csproj
    #	src/Transport/NServiceBus.Transport.AzureServiceBus.csproj
    #	src/TransportTests/NServiceBus.Transport.AzureServiceBus.TransportTests.csproj

--- src/Tests/FakeReceiver.cs
+++ src/Tests/FakeReceiver.cs
@@ -1,5 +1,6 @@
 namespace NServiceBus.Transport.AzureServiceBus.Tests
 {
+    using System;
     using System.Collections.Generic;
     using System.Threading;
     using System.Threading.Tasks;
@@ -9,12 +10,18 @@ namespace NServiceBus.Transport.AzureServiceBus.Tests
     {
         readonly List<(ServiceBusReceivedMessage, IDictionary<string, object> propertiesToModify)> abandonedMessages = new();
         readonly List<ServiceBusReceivedMessage> completedMessages = new();
+        readonly List<ServiceBusReceivedMessage> completingMessages = new();
+
+        public Func<ServiceBusReceivedMessage, CancellationToken, Task> CompleteMessageCallback = (_, _) => Task.CompletedTask;
 
         public IReadOnlyCollection<(ServiceBusReceivedMessage, IDictionary<string, object> propertiesToModify)> AbandonedMessages
             => abandonedMessages;
 
         public IReadOnlyCollection<ServiceBusReceivedMessage> CompletedMessages
             => completedMessages;
+
+        public IReadOnlyCollection<ServiceBusReceivedMessage> CompletingMessages
+            => completingMessages;
 
         public override Task AbandonMessageAsync(ServiceBusReceivedMessage message, IDictionary<string, object> propertiesToModify = null,
             CancellationToken cancellationToken = default)
@@ -23,11 +30,12 @@ namespace NServiceBus.Transport.AzureServiceBus.Tests
             return Task.CompletedTask;
         }
 
-        public override Task CompleteMessageAsync(ServiceBusReceivedMessage message,
+        public override async Task CompleteMessageAsync(ServiceBusReceivedMessage message,
             CancellationToken cancellationToken = default)
         {
+            completingMessages.Add(message);
+            await CompleteMessageCallback(message, cancellationToken);
             completedMessages.Add(message);
-            return Task.CompletedTask;
         }
     }
 }
