a671239af6c531ec2194b0b1b339d1146dc0deb0
Ramon Smits <ramon.smits@gmail.com>
2024-10-24 19:03:44 +0200

    Replaced delegate with global using

--- src/Transport/NativeMessageCustomizerCallback.cs
+++ src/Transport/NativeMessageCustomizerCallback.cs
@@ -1,7 +1,1 @@
-using Azure.Messaging.ServiceBus;
-using NServiceBus.Transport;
-
-/// <summary>
-/// Allow to customize the native ServiceBusMessage just before it is dispatched to the Azure Service Bus SDK client
-/// </summary>
-public delegate void NativeMessageCustomizerCallback(IOutgoingTransportOperation message, ServiceBusMessage nativeMessage);
+global using NativeMessageCustomizerCallback = System.Action<NServiceBus.Transport.IOutgoingTransportOperation, Azure.Messaging.ServiceBus.ServiceBusMessage>;
