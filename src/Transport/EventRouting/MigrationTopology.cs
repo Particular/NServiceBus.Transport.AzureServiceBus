cd2460988558bb52a7c89f163424d95fc9f7f3dc
Daniel Marbach <daniel.marbach@openplace.net>
2025-06-16 21:59:17 +0200

    Align MigrateFromNamedSingleTopic obsolete version range (#1209)
    * Align MigrateFromNamedSingleTopic obsolete version range with the other obsoletes since that is an oversight from a bumping PR.
    
    Introduces also constants for the version range with comments to better indicate the intent.
    
    * Use constants in MigrationTopology class also
    
    ---------
    
    Co-authored-by: Daniel Marbach <danielmarbach@users.noreply.github.com>
    Co-authored-by: Brandon Ording <bording@gmail.com>

--- src/Transport/EventRouting/MigrationTopology.cs
+++ src/Transport/EventRouting/MigrationTopology.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Options;
 /// <summary>
 /// Topology that allows mixing of single-topic and topic-per-event approaches in order to allow gradual migration to the topic-per-event topology.
 /// </summary>
-[ObsoleteEx(Message = ObsoleteMessage, TreatAsErrorFromVersion = "7", RemoveInVersion = "8")]
+[ObsoleteEx(Message = ObsoleteMessage, TreatAsErrorFromVersion = TreatAsErrorFromVersion, RemoveInVersion = RemoveInVersion)]
 public sealed class MigrationTopology : TopicTopology
 {
     internal MigrationTopology(MigrationTopologyOptions options) : base(options,
@@ -186,6 +186,17 @@ public sealed class MigrationTopology : TopicTopology
     }
 
     internal override SubscriptionManager CreateSubscriptionManager(SubscriptionManagerCreationOptions creationOptions) => new MigrationTopologySubscriptionManager(creationOptions, Options);
+
+    /*
+     * The idea behind obsoleting the migration topology is to make it clear in the user's code base that eventually the migration topology will be removed.
+     * The intent was to give users at least two major versions that are aligned with .NET LTS versions time, which should be roughly 2 years to migrate away
+     * from the migration topology to the new topic-per-event topology. Should an out-of-band major release be required, consider bumping the `TreatAsErrorFromVersion`
+     * and `RemoveInVersion` values to make sure more or least two years have passed after the release v5.0.0 of the transport. It might also be worthwhile
+     * checking the linked issue in the obsolete message to see if there are any signals that warrant moving this even further.
+     */
+    internal const string TreatAsErrorFromVersion = "7";
+
+    internal const string RemoveInVersion = "8";
 
     internal const string ObsoleteMessage =
         "The migration topology is intended to be used during a transitional period, facilitating the migration from the single-topic topology to the topic-per-event topology. The migration topology will eventually be phased out over subsequent releases. Should you face challenges during migration, please reach out to |https://github.com/Particular/NServiceBus.Transport.AzureServiceBus/issues/1170|.";
