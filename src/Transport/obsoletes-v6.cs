e97cc5f959ce0553b9ab1873d7487729e7e24d78
Ryan Thomas <ryan.thomas@particular.net>
2025-06-18 16:13:04 -0500

    Use File-scoped namespace declaration

--- src/Transport/obsoletes-v6.cs
+++ src/Transport/obsoletes-v6.cs
@@ -1,30 +1,29 @@
 ﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
-namespace NServiceBus
-{
-    using System;
+namespace NServiceBus;
 
-    public partial class AzureServiceBusTransport
+using System;
+
+public partial class AzureServiceBusTransport
+{
+    [ObsoleteEx(Message = "The transport no longer supports sending the transport encoding header.",
+    TreatAsErrorFromVersion = "6",
+    RemoveInVersion = "7")]
+    public bool SendTransportEncodingHeader
     {
-        [ObsoleteEx(Message = "The transport no longer supports sending the transport encoding header.",
+        get => throw new NotImplementedException();
+        set => throw new NotImplementedException();
+    }
+}
+
+public static partial class AzureServiceBusTransportSettingsExtensions
+{
+    [ObsoleteEx(
+        Message = "The transport no longer supports sending the transport encoding header.",
         TreatAsErrorFromVersion = "6",
         RemoveInVersion = "7")]
-        public bool SendTransportEncodingHeader
-        {
-            get => throw new NotImplementedException();
-            set => throw new NotImplementedException();
-        }
-    }
-
-    public static partial class AzureServiceBusTransportSettingsExtensions
-    {
-        [ObsoleteEx(
-            Message = "The transport no longer supports sending the transport encoding header.",
-            TreatAsErrorFromVersion = "6",
-            RemoveInVersion = "7")]
-        public static TransportExtensions<AzureServiceBusTransport> SendTransportEncodingHeader(
-            this TransportExtensions<AzureServiceBusTransport> transportExtensions) => throw new NotImplementedException();
-    }
+    public static TransportExtensions<AzureServiceBusTransport> SendTransportEncodingHeader(
+        this TransportExtensions<AzureServiceBusTransport> transportExtensions) => throw new NotImplementedException();
 }
 
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
