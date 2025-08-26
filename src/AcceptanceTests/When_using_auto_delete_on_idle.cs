378cb78954e32c1a061b933395c927a8855a5fe8
Warwick Schroeder <warwick.schroeder@particular.net>
2025-08-26 08:33:17 +0800

    Add additional tests

--- src/AcceptanceTests/When_using_auto_delete_on_idle.cs
+++ src/AcceptanceTests/When_using_auto_delete_on_idle.cs
@@ -11,7 +11,10 @@
 
     public class When_using_auto_delete_on_idle : NServiceBusAcceptanceTest
     {
-        const string ResultingEndpointInstanceName = "usingautodeleteonidle.endpointwithautodeleteonidle-12345";
+        const string HasAutoDeleteOnIdleEndpointInstanceName = "usingautodeleteonidle.endpointwithautodeleteonidle-12345";
+        const string NoAutoDeleteOnIdleEndpointInstanceName = "usingautodeleteonidle.endpointwithoutautodeleteonidle";
+        const string HasAutoDeleteOnIdleButNoInstancesEndpointName = "usingautodeleteonidle.endpointwithautodeleteonidlebutnoinstances";
+
         [SetUp]
         public async Task Setup()
         {
@@ -21,7 +24,9 @@
             try
             {
                 // makes sure during local development the topic gets cleared before each test run
-                await adminClient.DeleteQueueAsync(ResultingEndpointInstanceName);
+                await adminClient.DeleteQueueAsync(HasAutoDeleteOnIdleEndpointInstanceName);
+                await adminClient.DeleteQueueAsync(NoAutoDeleteOnIdleEndpointInstanceName);
+                await adminClient.DeleteQueueAsync(HasAutoDeleteOnIdleButNoInstancesEndpointName);
             }
             catch (ServiceBusException ex) when (ex.Reason == ServiceBusFailureReason.MessagingEntityNotFound)
             {
@@ -49,10 +54,58 @@
             var adminClient = new ServiceBusAdministrationClient(
                 Environment.GetEnvironmentVariable("AzureServiceBus_ConnectionString"));
 
-            var queueProperties = await adminClient.GetQueueAsync(ResultingEndpointInstanceName);
+            var queueProperties = await adminClient.GetQueueAsync(HasAutoDeleteOnIdleEndpointInstanceName);
 
             Assert.That(queueProperties.Value.AutoDeleteOnIdle, Is.EqualTo(TimeSpan.FromMinutes(10)));
         }
+
+        [Test]
+        public async Task Should_not_configure_queue_with_auto_delete_on_idle()
+        {
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<EndpointWithoutAutoDeleteOnIdle>(b =>
+                {
+                    b.CustomConfig(c =>
+                    {
+                        c.ConfigureTransport<AzureServiceBusTransport>();
+                    });
+                })
+                .Done(c => c.EndpointsStarted)
+                .Run();
+
+            // Verify that the queue was created with the correct AutoDeleteOnIdle setting
+            var adminClient = new ServiceBusAdministrationClient(
+                Environment.GetEnvironmentVariable("AzureServiceBus_ConnectionString"));
+
+            var queueProperties = await adminClient.GetQueueAsync(NoAutoDeleteOnIdleEndpointInstanceName);
+
+            Assert.That(queueProperties.Value.AutoDeleteOnIdle, Is.EqualTo(TimeSpan.MaxValue));
+        }
+
+        [Test]
+        public async Task Should_not_configure_queue_with_auto_delete_on_idle_if_no_unique_instances()
+        {
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<EndpointWithAutoDeleteOnIdleButNoInstances>(b =>
+                {
+                    b.CustomConfig(c =>
+                    {
+                        var transport = c.ConfigureTransport<AzureServiceBusTransport>();
+                        transport.AutoDeleteOnIdle = TimeSpan.FromMinutes(10);
+                    });
+                })
+                .Done(c => c.EndpointsStarted)
+                .Run();
+
+            // Verify that the queue was created with the correct AutoDeleteOnIdle setting
+            var adminClient = new ServiceBusAdministrationClient(
+                Environment.GetEnvironmentVariable("AzureServiceBus_ConnectionString"));
+
+            var queueProperties = await adminClient.GetQueueAsync(HasAutoDeleteOnIdleButNoInstancesEndpointName);
+
+            Assert.That(queueProperties.Value.AutoDeleteOnIdle, Is.EqualTo(TimeSpan.MaxValue));
+        }
+
 
         public class Context : ScenarioContext
         {
@@ -61,6 +114,22 @@
         public class EndpointWithAutoDeleteOnIdle : EndpointConfigurationBuilder
         {
             public EndpointWithAutoDeleteOnIdle()
+            {
+                EndpointSetup<DefaultServer>();
+            }
+        }
+
+        public class EndpointWithoutAutoDeleteOnIdle : EndpointConfigurationBuilder
+        {
+            public EndpointWithoutAutoDeleteOnIdle()
+            {
+                EndpointSetup<DefaultServer>();
+            }
+        }
+
+        public class EndpointWithAutoDeleteOnIdleButNoInstances : EndpointConfigurationBuilder
+        {
+            public EndpointWithAutoDeleteOnIdleButNoInstances()
             {
                 EndpointSetup<DefaultServer>();
             }
