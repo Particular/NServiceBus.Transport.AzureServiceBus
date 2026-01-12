5e3c4da189f16673ad1a052549b987618f1c56b7
Bartek Wasielak <bartek.wasielak@particular.net>
2026-01-12 16:41:29 -0500

    Added a name generator idempotence test

---
+++ src/Tests/QueueNameGeneratorTests.cs
@@ -0,0 +1,20 @@
+namespace NServiceBus.Transport.AzureServiceBus.Tests;
+
+using AzureServiceBus.Sending;
+using NUnit.Framework;
+
+[TestFixture]
+public class QueueNameGeneratorTests
+{
+    [Test]
+    public void Default_queue_name_generator_is_idempotent()
+    {
+        const string prefix = "Prefix";
+        const string destination = "Destination";
+
+        var once = destination.ToHierarchyNamespaceAwareDestination(prefix);
+        var twice = once.ToHierarchyNamespaceAwareDestination(prefix);
+
+        Assert.That(twice, Is.EqualTo(once), "Applying the default queue name generator twice should impact the outcome");
+    }
+}
