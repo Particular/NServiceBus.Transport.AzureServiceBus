b02d12d00d8b538889eaf354d2c64d71c1eda4f8
Nick Gallegos <nick.gallegos@particular.net>
2026-01-20 11:00:42 -0700

    Use string.Create in ToHierarchyNamespaceAwareDestination.

--- src/Transport/Sending/HierarchyNamespaceExtensions.cs
+++ src/Transport/Sending/HierarchyNamespaceExtensions.cs
@@ -1,14 +1,24 @@
 namespace NServiceBus.Transport.AzureServiceBus.Sending;
 
+using System;
+
 static class HierarchyNamespaceExtensions
 {
     internal static string ToHierarchyNamespaceAwareDestination(this string destination, string? hierarchyNamespace)
     {
-        if (hierarchyNamespace is null || destination.StartsWith(hierarchyNamespace))
+        if (hierarchyNamespace is null || destination.StartsWith(hierarchyNamespace + "/", StringComparison.Ordinal))
         {
             return destination;
         }
 
-        return $"{hierarchyNamespace}/{destination}";
+        return string.Create(hierarchyNamespace.Length + 1 + destination.Length, (hierarchyNamespace, destination), static (chars, state) =>
+        {
+            var position = 0;
+            (string hierarchyNamespace, string destination) = state;
+            hierarchyNamespace.AsSpan().CopyTo(chars);
+            position += hierarchyNamespace.Length;
+            chars[position++] = '/';
+            destination.AsSpan().CopyTo(chars.Slice(position));
+        });
     }
 }
