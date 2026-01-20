6e533f79fc2c908c3ce1376a13cbc9e80bd4938c
Nick Gallegos <nick.gallegos@particular.net>
2026-01-20 11:35:25 -0700

    Use spans for efficiency in ToHierarchicalNamespaceAwareDestination.

--- src/Transport/Sending/HierarchyNamespaceExtensions.cs
+++ src/Transport/Sending/HierarchyNamespaceExtensions.cs
@@ -6,17 +6,19 @@ static class HierarchyNamespaceExtensions
 {
     internal static string ToHierarchyNamespaceAwareDestination(this string destination, string? hierarchyNamespace)
     {
-        if (hierarchyNamespace is null || destination.StartsWith(hierarchyNamespace + "/", StringComparison.Ordinal))
+        var hierarchyNamespaceSpan = hierarchyNamespace.AsSpan();
+        var destinationSpan = destination.AsSpan();
+        if (hierarchyNamespaceSpan.IsEmpty || destinationSpan.StartsWith(hierarchyNamespaceSpan, StringComparison.Ordinal))
         {
             return destination;
         }
 
-        return string.Create(hierarchyNamespace.Length + 1 + destination.Length, (hierarchyNamespace, destination), static (chars, state) =>
+        return string.Create(hierarchyNamespaceSpan.Length + 1 + destinationSpan.Length, (hierarchyNamespace, destination), static (chars, state) =>
         {
             var position = 0;
-            (string hierarchyNamespace, string destination) = state;
+            (string? hierarchyNamespace, string destination) = state;
             hierarchyNamespace.AsSpan().CopyTo(chars);
-            position += hierarchyNamespace.Length;
+            position += hierarchyNamespace?.Length ?? 0;
             chars[position++] = '/';
             destination.AsSpan().CopyTo(chars.Slice(position));
         });
