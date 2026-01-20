762ea7ddc99275d5321dab28dfcaf663e9558137
Nick Gallegos <nick.gallegos@particular.net>
2026-01-20 11:59:01 -0700

    Refactor for hierarchy namespace ending consistency

--- src/Transport/Sending/HierarchyNamespaceExtensions.cs
+++ src/Transport/Sending/HierarchyNamespaceExtensions.cs
@@ -6,19 +6,19 @@ static class HierarchyNamespaceExtensions
 {
     internal static string ToHierarchyNamespaceAwareDestination(this string destination, string? hierarchyNamespace)
     {
-        var hierarchyNamespaceSpan = hierarchyNamespace.AsSpan();
+        var hierarchyNamespaceSpan = string.Concat(hierarchyNamespace, '/').AsSpan();
         var destinationSpan = destination.AsSpan();
-        if (hierarchyNamespaceSpan.IsEmpty || destinationSpan.StartsWith(hierarchyNamespaceSpan, StringComparison.Ordinal))
+        if (string.IsNullOrEmpty(hierarchyNamespace) || destinationSpan.StartsWith(hierarchyNamespaceSpan, StringComparison.Ordinal))
         {
             return destination;
         }
 
-        return string.Create(hierarchyNamespaceSpan.Length + 1 + destinationSpan.Length, (hierarchyNamespace, destination), static (chars, state) =>
+        return string.Create(hierarchyNamespace.Length + 1 + destination.Length, (hierarchyNamespace, destination), static (chars, state) =>
         {
             var position = 0;
             (string? hierarchyNamespace, string destination) = state;
             hierarchyNamespace.AsSpan().CopyTo(chars);
-            position += hierarchyNamespace?.Length ?? 0;
+            position += hierarchyNamespace.Length;
             chars[position++] = '/';
             destination.AsSpan().CopyTo(chars.Slice(position));
         });
