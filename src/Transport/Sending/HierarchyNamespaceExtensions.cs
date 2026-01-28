f154c8d0b6a56ecf0d742b4e1a517112a5bd6167
Nick Gallegos <nick.gallegos@particular.net>
2026-01-27 22:17:50 -0700

    Cleanup

--- src/Transport/Sending/HierarchyNamespaceExtensions.cs
+++ src/Transport/Sending/HierarchyNamespaceExtensions.cs
@@ -4,26 +4,6 @@ using System;
 
 static class HierarchyNamespaceExtensions
 {
-    internal static string ToHierarchyNamespaceAwareDestination(this string destination, string? hierarchyNamespace)
-    {
-        var hierarchyNamespaceSpan = string.Concat(hierarchyNamespace, '/').AsSpan();
-        var destinationSpan = destination.AsSpan();
-        if (string.IsNullOrEmpty(hierarchyNamespace) || destinationSpan.StartsWith(hierarchyNamespaceSpan, StringComparison.Ordinal))
-        {
-            return destination;
-        }
-
-        return string.Create(hierarchyNamespace.Length + 1 + destination.Length, (hierarchyNamespace, destination), static (chars, state) =>
-        {
-            var position = 0;
-            (string? hierarchyNamespace, string destination) = state;
-            hierarchyNamespace.AsSpan().CopyTo(chars);
-            position += hierarchyNamespace.Length;
-            chars[position++] = '/';
-            destination.AsSpan().CopyTo(chars.Slice(position));
-        });
-    }
-
     internal static string ToHierarchyNamespaceAwareDestination(this string destination, HierarchyNamespaceOptions? hierarchyNamespaceOptions, string? messageTypeFullName = null)
     {
         if (string.IsNullOrWhiteSpace(hierarchyNamespaceOptions?.HierarchyNamespace))
