5bd8ac53efd638fe52cc74153ac9170c176bb09a
Daniel Marbach <danielmarbach@users.noreply.github.com>
2026-02-06 17:24:16 +0100

    Refactor test to use file-scoped namespace and improve formatting consistency.

--- src/AcceptanceTests/Sending/When_using_hierarchy_namespace.cs
+++ src/AcceptanceTests/Sending/When_using_hierarchy_namespace.cs
@@ -1,147 +1,146 @@
-namespace NServiceBus.Transport.AzureServiceBus.AcceptanceTests.Sending
+namespace NServiceBus.Transport.AzureServiceBus.AcceptanceTests.Sending;
+
+using System;
+using System.Threading.Tasks;
+using AcceptanceTesting;
+using Conventions = NServiceBus.AcceptanceTesting.Customization.Conventions;
+using NServiceBus.AcceptanceTests;
+using NServiceBus.AcceptanceTests.EndpointTemplates;
+using NUnit.Framework;
+
+public class When_using_hierarchy_namespace : NServiceBusAcceptanceTest
 {
-    using System;
-    using System.Threading.Tasks;
-    using AcceptanceTesting;
-    using Conventions = NServiceBus.AcceptanceTesting.Customization.Conventions;
-    using NServiceBus.AcceptanceTests;
-    using NServiceBus.AcceptanceTests.EndpointTemplates;
-    using NUnit.Framework;
-
-    public class When_using_hierarchy_namespace : NServiceBusAcceptanceTest
+    [Test]
+    public async Task Should_send_and_receive_with_hierarchy_namespace()
     {
-        [Test]
-        public async Task Should_send_and_receive_with_hierarchy_namespace()
-        {
-            var context = await Scenario.Define<Context>()
-                .WithEndpoint<Sender>(endpoint =>
-                {
-                    endpoint.CustomConfig(cfg =>
-                    {
-                        var transport = cfg.ConfigureTransport<AzureServiceBusTransport>();
-                        transport.HierarchyNamespaceOptions = new HierarchyNamespaceOptions { HierarchyNamespace = "my-hierarchy" };
-                    });
-
-                    endpoint.When(async session => await session.Send(Conventions.EndpointNamingConvention(typeof(HierarchyReceiver)).Shorten(), new MyMessage()));
-                })
-                .WithEndpoint<ExternalReceiver>()
-                .WithEndpoint<HierarchyReceiver>(endpoint =>
-                {
-                    endpoint.CustomConfig(cfg =>
-                    {
-                        var transport = cfg.ConfigureTransport<AzureServiceBusTransport>();
-                        transport.HierarchyNamespaceOptions = new HierarchyNamespaceOptions { HierarchyNamespace = "my-hierarchy" };
-                    });
-                })
-                .Run();
-
-            using (Assert.EnterMultipleScope())
+        var context = await Scenario.Define<Context>()
+            .WithEndpoint<Sender>(endpoint =>
             {
-                Assert.That(context.HierarchyMessageReceived, Is.True);
-                Assert.That(context.ExternalMessageReceived, Is.False);
-                Assert.That(context.ExternalEventReceived, Is.False);
-            }
-        }
-
-        [Test]
-        public async Task Should_publish_and_receive_with_hierarchy_namespace()
-        {
-            var context = await Scenario.Define<Context>()
-                .WithEndpoint<Sender>(endpoint =>
+                endpoint.CustomConfig(cfg =>
                 {
-                    endpoint.CustomConfig(cfg =>
-                    {
-                        var transport = cfg.ConfigureTransport<AzureServiceBusTransport>();
-                        transport.HierarchyNamespaceOptions = new HierarchyNamespaceOptions { HierarchyNamespace = "my-hierarchy" };
-                    });
-                    endpoint.When(async session => await session.Publish(new MyEvent()));
-                })
-                .WithEndpoint<ExternalReceiver>()
-                .WithEndpoint<HierarchyReceiver>(endpoint =>
-                {
-                    endpoint.CustomConfig(cfg =>
-                    {
-                        var transport = cfg.ConfigureTransport<AzureServiceBusTransport>();
-                        transport.HierarchyNamespaceOptions = new HierarchyNamespaceOptions { HierarchyNamespace = "my-hierarchy" };
-                    });
-                })
-                .Run();
+                    var transport = cfg.ConfigureTransport<AzureServiceBusTransport>();
+                    transport.HierarchyNamespaceOptions = new HierarchyNamespaceOptions { HierarchyNamespace = "my-hierarchy" };
+                });
 
-            using (Assert.EnterMultipleScope())
+                endpoint.When(async session => await session.Send(Conventions.EndpointNamingConvention(typeof(HierarchyReceiver)).Shorten(), new MyMessage()));
+            })
+            .WithEndpoint<ExternalReceiver>()
+            .WithEndpoint<HierarchyReceiver>(endpoint =>
             {
-                Assert.That(context.HierarchyEventReceived, Is.True);
-                Assert.That(context.ExternalMessageReceived, Is.False);
-                Assert.That(context.ExternalEventReceived, Is.False);
-            }
-        }
-
-        public class Context : ScenarioContext
-        {
-            public bool HierarchyMessageReceived { get; set; }
-            public bool HierarchyEventReceived { get; set; }
-            public bool ExternalMessageReceived { get; set; }
-            public bool ExternalEventReceived { get; set; }
-        }
-
-        class Sender : EndpointConfigurationBuilder
-        {
-            public Sender() => EndpointSetup<DefaultServer>(
-                _ => { },
-                publishMetadata => publishMetadata.RegisterSelfAsPublisherFor<MyEvent>(this)
-            );
-        }
-
-        class HierarchyReceiver : EndpointConfigurationBuilder
-        {
-            public HierarchyReceiver() => EndpointSetup<DefaultServer>(
-                _ => { },
-                publishMetadata => publishMetadata.RegisterPublisherFor<MyEvent, Sender>());
-
-            public class MyHandler(Context testContext) : IHandleMessages<MyMessage>, IHandleMessages<MyEvent>
-            {
-                public Task Handle(MyMessage message, IMessageHandlerContext context)
+                endpoint.CustomConfig(cfg =>
                 {
-                    testContext.HierarchyMessageReceived = true;
-                    testContext.MarkAsCompleted();
-                    return Task.CompletedTask;
-                }
+                    var transport = cfg.ConfigureTransport<AzureServiceBusTransport>();
+                    transport.HierarchyNamespaceOptions = new HierarchyNamespaceOptions { HierarchyNamespace = "my-hierarchy" };
+                });
+            })
+            .Run();
 
-                public Task Handle(MyEvent message, IMessageHandlerContext context)
-                {
-                    testContext.ExternalEventReceived = true;
-                    testContext.MarkAsCompleted();
-                    return Task.CompletedTask;
-                }
-            }
-        }
-
-        class ExternalReceiver : EndpointConfigurationBuilder
+        using (Assert.EnterMultipleScope())
         {
-            public ExternalReceiver() => EndpointSetup<DefaultServer>(
-                _ => { },
-                publishMetadata => publishMetadata.RegisterPublisherFor<MyEvent, Sender>()
-            );
-
-            public class MyMessageHandler(Context testContext) : IHandleMessages<MyMessage>, IHandleMessages<MyEvent>
-            {
-                public Task Handle(MyMessage message, IMessageHandlerContext context)
-                {
-                    testContext.ExternalMessageReceived = true;
-                    testContext.MarkAsFailed(new Exception("External receiver should not receive the hierarchy message"));
-                    return Task.CompletedTask;
-                }
-
-                public Task Handle(MyEvent message, IMessageHandlerContext context)
-                {
-                    testContext.ExternalMessageReceived = true;
-                    testContext.MarkAsFailed(new Exception("External receiver should not receive the hierarchy event"));
-                    return Task.CompletedTask;
-                }
-            }
+            Assert.That(context.HierarchyMessageReceived, Is.True);
+            Assert.That(context.ExternalMessageReceived, Is.False);
+            Assert.That(context.ExternalEventReceived, Is.False);
         }
-
-        public class MyMessage : ICommand;
-
-        public class MyEvent : IEvent;
     }
+
+    [Test]
+    public async Task Should_publish_and_receive_with_hierarchy_namespace()
+    {
+        var context = await Scenario.Define<Context>()
+            .WithEndpoint<Sender>(endpoint =>
+            {
+                endpoint.CustomConfig(cfg =>
+                {
+                    var transport = cfg.ConfigureTransport<AzureServiceBusTransport>();
+                    transport.HierarchyNamespaceOptions = new HierarchyNamespaceOptions { HierarchyNamespace = "my-hierarchy" };
+                });
+                endpoint.When(async session => await session.Publish(new MyEvent()));
+            })
+            .WithEndpoint<ExternalReceiver>()
+            .WithEndpoint<HierarchyReceiver>(endpoint =>
+            {
+                endpoint.CustomConfig(cfg =>
+                {
+                    var transport = cfg.ConfigureTransport<AzureServiceBusTransport>();
+                    transport.HierarchyNamespaceOptions = new HierarchyNamespaceOptions { HierarchyNamespace = "my-hierarchy" };
+                });
+            })
+            .Run();
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(context.HierarchyEventReceived, Is.True);
+            Assert.That(context.ExternalMessageReceived, Is.False);
+            Assert.That(context.ExternalEventReceived, Is.False);
+        }
+    }
+
+    public class Context : ScenarioContext
+    {
+        public bool HierarchyMessageReceived { get; set; }
+        public bool HierarchyEventReceived { get; set; }
+        public bool ExternalMessageReceived { get; set; }
+        public bool ExternalEventReceived { get; set; }
+    }
+
+    class Sender : EndpointConfigurationBuilder
+    {
+        public Sender() => EndpointSetup<DefaultServer>(
+            _ => { },
+            publishMetadata => publishMetadata.RegisterSelfAsPublisherFor<MyEvent>(this)
+        );
+    }
+
+    class HierarchyReceiver : EndpointConfigurationBuilder
+    {
+        public HierarchyReceiver() => EndpointSetup<DefaultServer>(
+            _ => { },
+            publishMetadata => publishMetadata.RegisterPublisherFor<MyEvent, Sender>());
+
+        public class MyHandler(Context testContext) : IHandleMessages<MyMessage>, IHandleMessages<MyEvent>
+        {
+            public Task Handle(MyMessage message, IMessageHandlerContext context)
+            {
+                testContext.HierarchyMessageReceived = true;
+                testContext.MarkAsCompleted();
+                return Task.CompletedTask;
+            }
+
+            public Task Handle(MyEvent message, IMessageHandlerContext context)
+            {
+                testContext.ExternalEventReceived = true;
+                testContext.MarkAsCompleted();
+                return Task.CompletedTask;
+            }
+        }
+    }
+
+    class ExternalReceiver : EndpointConfigurationBuilder
+    {
+        public ExternalReceiver() => EndpointSetup<DefaultServer>(
+            _ => { },
+            publishMetadata => publishMetadata.RegisterPublisherFor<MyEvent, Sender>()
+        );
+
+        public class MyMessageHandler(Context testContext) : IHandleMessages<MyMessage>, IHandleMessages<MyEvent>
+        {
+            public Task Handle(MyMessage message, IMessageHandlerContext context)
+            {
+                testContext.ExternalMessageReceived = true;
+                testContext.MarkAsFailed(new Exception("External receiver should not receive the hierarchy message"));
+                return Task.CompletedTask;
+            }
+
+            public Task Handle(MyEvent message, IMessageHandlerContext context)
+            {
+                testContext.ExternalMessageReceived = true;
+                testContext.MarkAsFailed(new Exception("External receiver should not receive the hierarchy event"));
+                return Task.CompletedTask;
+            }
+        }
+    }
+
+    public class MyMessage : ICommand;
+
+    public class MyEvent : IEvent;
 }
