5bdf3b43a38480af4040434cf222ffc3ecd2edf5
Andreas Öhlund <andreas.ohlund@particular.net>
2026-04-04 13:12:17 +0200

    Simplify

--- src/Transport/Receiving/DeadLetterRequest.cs
+++ src/Transport/Receiving/DeadLetterRequest.cs
@@ -3,12 +3,29 @@ namespace NServiceBus.Transport.AzureServiceBus;
 using System;
 using System.Collections.Generic;
 
-class DeadLetterRequest(string deadLetterReason, string deadLetterErrorDescription, Dictionary<string, object>? propertiesToModify = null)
+/// <summary>
+/// Requests the transport to dlq the message being processed when added to the transport transacton exposed to onError.
+/// </summary>
+public class DeadLetterRequest(string deadLetterReason, string deadLetterErrorDescription, Dictionary<string, object>? propertiesToModify = null)
 {
+    /// <summary>
+    /// Reason for the message being dead lettered, truncated to max 1024 characters.
+    /// </summary>
     public string DeadLetterReason { get; } = Truncate(deadLetterReason, 1024);
+
+    /// <summary>
+    /// Additional details for the message being dead lettered, truncated to max 1024 characters.
+    /// </summary>
     public string DeadLetterErrorDescription { get; } = Truncate(deadLetterErrorDescription, 1024);
+
+    /// <summary>
+    /// Properties to update on the message to be dead lettered.
+    /// </summary>
     public Dictionary<string, object> PropertiesToModify { get; } = propertiesToModify ?? [];
 
+    /// <summary>
+    /// Dead letters the message being processed due to the provided exception.
+    /// </summary>
     public DeadLetterRequest(Exception exception, Dictionary<string, object>? propertiesToModify = null) : this(
         $"{exception.GetType().FullName} - {exception.Message}",
         exception.StackTrace ?? "No stack trace available",
