92a29735b5703def95d429bb8f751cac5e5b4c62
Poornima Nayar <poornima.nayar@particular.net>
2026-07-14 13:43:06 +0100

    Reintroduced the topology mapping for polymorphic events for the session enabled acceptance tests

--- src/NServiceBus.Transport.AzureServiceBus.AcceptanceTests.SessionEnabled/ConfigureEndpointAzureServiceBusTransport.cs
+++ src/NServiceBus.Transport.AzureServiceBus.AcceptanceTests.SessionEnabled/ConfigureEndpointAzureServiceBusTransport.cs
@@ -4,9 +4,15 @@ using System.Threading.Tasks;
 using NServiceBus;
 using NServiceBus.AcceptanceTesting.Customization;
 using NServiceBus.AcceptanceTesting.Support;
+using NServiceBus.AcceptanceTests.Routing;
+using NServiceBus.AcceptanceTests.Routing.NativePublishSubscribe;
+using NServiceBus.AcceptanceTests.Sagas;
+using NServiceBus.AcceptanceTests.Versioning;
 using NServiceBus.Pipeline;
 using NServiceBus.Transport;
+using NServiceBus.Transport.AzureServiceBus;
 using NServiceBus.Transport.AzureServiceBus.AcceptanceTests;
+using Conventions = NServiceBus.AcceptanceTesting.Customization.Conventions;
 
 public class GenerateRandomSessionIdForSends : Behavior<IOutgoingSendContext>
 {
@@ -79,6 +85,8 @@ public class ConfigureEndpointAzureServiceBusTransport : IConfigureEndpointTestE
             transport.EnableSessions = true;
         }
 
+        ApplyMappingsToSupportMultipleInheritance(endpointName, topology);
+
         configuration.UseTransport(transport);
 
         configuration.EnableTestIndependence();
@@ -90,6 +98,43 @@ public class ConfigureEndpointAzureServiceBusTransport : IConfigureEndpointTestE
         configuration.EnforcePublisherMetadataRegistration(endpointName, publisherMetadata);
 
         return Task.CompletedTask;
+    }
+
+    static void ApplyMappingsToSupportMultipleInheritance(string endpointName, TopicPerEventTopology topology)
+    {
+        if (endpointName == Conventions.EndpointNamingConvention(typeof(MultiSubscribeToPolymorphicEvent.Subscriber)))
+        {
+            topology.SubscribeTo<MultiSubscribeToPolymorphicEvent.IMyEvent>(typeof(MultiSubscribeToPolymorphicEvent.MyEvent1).ToTopicName());
+            topology.SubscribeTo<MultiSubscribeToPolymorphicEvent.IMyEvent>(typeof(MultiSubscribeToPolymorphicEvent.MyEvent2).ToTopicName());
+        }
+
+        if (endpointName == Conventions.EndpointNamingConvention(typeof(When_subscribing_to_a_base_event.GeneralSubscriber)))
+        {
+            topology.SubscribeTo<When_subscribing_to_a_base_event.IBaseEvent>(typeof(When_subscribing_to_a_base_event.SpecificEvent).ToTopicName());
+        }
+
+        if (endpointName == Conventions.EndpointNamingConvention(
+                typeof(When_publishing_an_event_implementing_two_unrelated_interfaces.Subscriber)))
+        {
+            topology.SubscribeTo<When_publishing_an_event_implementing_two_unrelated_interfaces.IEventA>(
+                typeof(When_publishing_an_event_implementing_two_unrelated_interfaces.CompositeEvent).ToTopicName());
+            topology.SubscribeTo<When_publishing_an_event_implementing_two_unrelated_interfaces.IEventB>(
+                typeof(When_publishing_an_event_implementing_two_unrelated_interfaces.CompositeEvent).ToTopicName());
+        }
+
+        if (endpointName == Conventions.EndpointNamingConvention(
+                typeof(When_started_by_base_event_from_other_saga.SagaThatIsStartedByABaseEvent)))
+        {
+            topology.SubscribeTo<When_started_by_base_event_from_other_saga.IBaseEvent>(
+                typeof(When_started_by_base_event_from_other_saga.ISomethingHappenedEvent).ToTopicName());
+        }
+
+        if (endpointName == Conventions.EndpointNamingConvention(
+                typeof(When_multiple_versions_of_a_message_is_published.V1Subscriber)))
+        {
+            topology.SubscribeTo<When_multiple_versions_of_a_message_is_published.V1Event>(
+                typeof(When_multiple_versions_of_a_message_is_published.V2Event).ToTopicName());
+        }
     }
 
     public Task Cleanup() => Task.CompletedTask;
