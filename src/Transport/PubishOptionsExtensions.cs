02f710a9a5a7d210216d99d1e3bc665620707bf6
Poornima Nayar <poornima.nayar@particular.net>
2026-07-15 13:02:46 +0100

    Added PublishOptions class to create an extension method for setting session id

---
+++ src/Transport/PubishOptionsExtensions.cs
@@ -0,0 +1,24 @@
+namespace NServiceBus;
+
+using System;
+using Extensibility;
+
+/// <summary>
+/// Extensions for <see cref="SendOptions"/>.
+/// </summary>
+public static class PubishOptionsExtensions
+{
+    /// <summary>
+    /// Extends the PublishOptions class to allow adding a session id to dispatch properties
+    /// </summary>
+    /// <param name="options">PublishOptions</param>
+    /// <param name="sessionId">The session id to add to the dispatch properties</param>
+    /// <exception cref="ArgumentException"></exception>
+    public static void SetSessionId(this PublishOptions options, string sessionId)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(sessionId);
+
+        var dispatchProperties = options.GetDispatchProperties();
+        dispatchProperties.TryAdd("SessionId", sessionId);
+    }
+}
